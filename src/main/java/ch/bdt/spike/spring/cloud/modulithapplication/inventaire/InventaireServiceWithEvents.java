15c7965a173ac258b69f49421953c4777842d954
Gagné Jean-Christian <jean-christian.gagne@ne.ch>
2026-06-15 08:56:35 +0200

    tuning

--- src/main/java/ch/bdt/spike/spring/cloud/modulithapplication/inventaire/InventaireServiceWithEvents.java
+++ src/main/java/ch/bdt/spike/spring/cloud/modulithapplication/inventaire/InventaireServiceWithEvents.java
@@ -1,7 +1,6 @@
 package ch.bdt.spike.spring.cloud.modulithapplication.inventaire;
 
 import ch.bdt.spike.spring.cloud.modulithapplication.commande.CommandeCompletedEvent;
-import ch.bdt.spike.spring.cloud.modulithapplication.inventaire.dao.StockDAO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -11,16 +10,20 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class InventaireServiceWithEvents {
-    private final StockDAO stockDAO;
 
-    //@ApplicationModuleListener
     @EventListener
     public void updateStockFor(CommandeCompletedEvent aEvent) {
         log.info("Updating stock for event {}", aEvent);
         if (aEvent.getCommandeId() == 2L) {
             throw new RuntimeException("boum");
         }
-        stockDAO.save();
+
+        try {
+            Thread.sleep(10 * 1000);
+        } catch (InterruptedException aE) {
+            throw new RuntimeException(aE);
+        }
+
         log.info("Stock updated for event {}", aEvent);
     }
 }
