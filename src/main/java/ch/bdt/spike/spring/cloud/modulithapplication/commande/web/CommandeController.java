20e9f1311edf97756f34c90711c3f8ff3602ff36
Gagné Jean-Christian <jean-christian.gagne@ne.ch>
2026-06-15 08:50:15 +0200

    tuning

--- src/main/java/ch/bdt/spike/spring/cloud/modulithapplication/commande/web/CommandeController.java
+++ src/main/java/ch/bdt/spike/spring/cloud/modulithapplication/commande/web/CommandeController.java
@@ -1,5 +1,6 @@
 package ch.bdt.spike.spring.cloud.modulithapplication.commande.web;
 
+import ch.bdt.spike.spring.cloud.modulithapplication.commande.CommandeService;
 import ch.bdt.spike.spring.cloud.modulithapplication.commande.CommandeServiceWithEvent;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
@@ -12,12 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommandeController {
 
     @Resource
-    private CommandeServiceWithEvent commandeServiceWithEvent;
+    private CommandeService commandeService;
 
     @GetMapping("/completeCommande/{id}")
     public String completeCommande(@PathVariable long id) {
         log.info("Completing command {}", id);
-        commandeServiceWithEvent.complete(id);
+        commandeService.complete(id);
         return "Commande completed: " + id;
     }
 }
